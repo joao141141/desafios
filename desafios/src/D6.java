f6590bf73985cc76b6bc304880c5ba3b52475f0b
joao <joaohenrique131@gmail.com>
2024-05-03 00:06:51 -0300

    Desafios parte 2.

---
+++ desafios/src/D6.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class D6 {
+    public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(System.in).useLocale(Locale.US)){
+
+            System.out.println("");
+            System.out.println("Bem vindo ao nosso verificador de Triangulos!!!");
+            System.out.println("Insira os lados do triangulo");
+
+            System.out.println("Lado A:");
+            double ladoA = scanner.nextDouble();
+            scanner.nextLine();
+
+
+            System.out.println("Lado B:");
+            double ladoB = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("Lado C:");
+            double ladoC = scanner.nextDouble();
+            scanner.nextLine();
+
+            if((ladoA + ladoB) > ladoC && (ladoA + ladoC) > ladoB && (ladoB + ladoC) > ladoA){
+                System.out.println("Triângulo válido");
+            }
+            else {
+                System.out.println("Triângulo Inválido");
+            }
+
+        }
+    }
+}
